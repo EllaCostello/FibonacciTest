1206d557a2be1212fadc15eb688c7515b9f2a4d1
Ella Costello <ella.costello@yahoo.com>
2026-01-26 20:27:25 +0100

    aaa

---
+++ src/main/java/com/hauxy/fibonnacitest/FibonnaciTestApplication.java
@@ -0,0 +1,13 @@
+package com.hauxy.fibonnacitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonnaciTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FibonnaciTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hauxy/fibonnacitest/FibonnaciTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hauxy.fibonnacitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonnaciTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
