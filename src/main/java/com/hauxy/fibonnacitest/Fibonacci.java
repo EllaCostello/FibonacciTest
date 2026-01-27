7d0ef9f7b9b4bf3e39089d08b63b08be53b2275d
Ella Costello <ella.costello@yahoo.com>
2026-01-27 09:57:51 +0100

    calcWithLoop

--- src/main/java/com/hauxy/fibonnacitest/Fibonacci.java
+++ src/main/java/com/hauxy/fibonnacitest/Fibonacci.java
@@ -2,40 +2,7 @@ package com.hauxy.fibonnacitest;
 
 public class Fibonacci {
 
-    // OPGAVE 1: Opfylder testen, men regner ikke korrekt
-    /*
-
-    public static int F(int n) {
-        if (n >= 2) {
-            return n -1;
-        }
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        return n;
-    }
-
-     */
-
-    // OPGAVE 2: Beregner Fibonacci-tal naivt ud fra definitionen
-    /*
-    public static int F(int n) {
-        if (n > 1) {
-            return F(n-1) + F(n-2);
-        }
-
-        if (n < 0) {
-            throw  new IllegalArgumentException();
-        }
-
-        return n;
-    }
-
-     */
-
     // OPGAVE 3: Beregner Fibonacci-tal ved brug af en løkke
-    /*
     public static int F(int n) {
         if (n < 0) {
             throw new IllegalArgumentException();
@@ -54,19 +21,4 @@ public class Fibonacci {
 
         return current;
     }
-     */
-
-    // OPGAVE 4: Beregner uden brug af løkker, men rekursiv
-    public static int F(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        } else if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return F(n - 1) + F(n - 2);
-        }
-    }
-
 }
