0c76142fabc2253b4cc6a0b3aadc3ec499f02ec3
Ella Costello <ella.costello@yahoo.com>
2026-01-28 10:21:14 +0100

    hh

--- src/main/java/com/hauxy/fibonnacitest/Fibonacci.java
+++ src/main/java/com/hauxy/fibonnacitest/Fibonacci.java
@@ -1,18 +1,28 @@
 package com.hauxy.fibonnacitest;
 
+import java.math.BigInteger;
+
 public class Fibonacci {
 
     // OPGAVE 4: Beregner uden brug af løkker, men rekursiv
     public static int F(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        } else if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+        if (n < 0) throw new IllegalArgumentException();
+
+        if (n == 0 || n == 1) {
+            return n;
         } else {
-            return F(n - 1) + F(n - 2);
+            return F_help(n, 2);
         }
+
+
+
+
     }
+
+    private static int F_help(int n, int k) {
+
+    }
+
+
 
 }
